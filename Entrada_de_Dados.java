098d8b151883edbe42df0c0539ae79709728158d
Marcos Kreuzberg <61471532+mKreuzberg@users.noreply.github.com>
2020-02-25 15:01:17 -0300

    Add files via upload

---
+++ Entrada_de_Dados.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Entrada_de_Dados {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String x;
+		System.out.println("Digita qualquer carai ae: ");
+		x = sc.next();
+		System.out.println("Tu digitou: "+x);
+		
+		
+		sc.close();
+	}
+
+}
